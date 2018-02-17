23ae4dce0cc5e3efe75df07da8c8e170e9d517c5
vivekbairi <36558667+vivekbairi@users.noreply.github.com>
2018-02-17 17:27:03 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,5 +7,8 @@ uptime
 df -Th
 mkdir /var/tmp/jenkins
 touch /var/tmp/jenkins/test1.txt
+ls -l /var/tmp/jenkins
 echo "End of script" > /var/tmp/jenkins/test1.txt
+cat /var/tmp/jenkins/test1.txt
+echo "End of script"
 echo "################################################"
