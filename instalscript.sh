fe2291189a6bf1405f53028b7e1949b68b4f562c
vivekbairi <36558667+vivekbairi@users.noreply.github.com>
2018-02-19 17:34:59 +0530

    Update instalscript.sh

--- instalscript.sh
+++ instalscript.sh
@@ -2,11 +2,15 @@
 echo "#######Script started#####"
 uptime
 date
-echo "To check know NFS status "
+echo "####To check know NFS status####"
 rpm -qa | grep -i nfs
+sudo su -
+techm123$
+date
 yum install -y nfs*
 systemctl status nfs
-systemctl start nfs 
+systemctl restart nfs 
+systemctl restart nfs
 systemctl status nfs
 echo "#######Script Ended#######"
 echo "##########################"
